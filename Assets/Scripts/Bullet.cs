25856a958dd54e9f33ccd4c66ad9d50c56fcc03c
Robby Klein <hello@robbyk.com>
2023-04-08 02:31:08 -0700

    explosion

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,18 +5,32 @@ public class Bullet : MonoBehaviour {
 
     float speed = 10f;
     Rigidbody2D rb;
+    Animator animator;
+    BoxCollider2D boxCollider;
 
     // Start is called before the first frame update
     void Start() {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
         rb.velocity = transform.up * speed;
     }
-
-
 
     void OnTriggerExit2D(Collider2D collision) {
         if (!boundryMask.IsInMask(collision.gameObject.layer)) {
             Destroy(gameObject);
         }
     }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        rb.velocity = Vector2.zero;
+        boxCollider.size = Vector2.zero;
+        animator.SetTrigger("Explode");
+    }
+
+    void DestroyBullet() {
+        Destroy(gameObject);
+    }
+
+
 }
